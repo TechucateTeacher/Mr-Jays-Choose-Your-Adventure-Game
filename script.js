fdff80c3d2569ff203b15d9e34402f8374316281
TechucateTeacher <123103785+TechucateTeacher@users.noreply.github.com>
2023-02-17 11:06:59 -0500

    Created using Popcode: https://popcode.org

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ let storyOpen = document.querySelector(".story-opening");
 let openButtons = document.querySelector(".open-buttons");
 let openScreenOne = document.querySelector(".open-one-screen");
 let openScreenTwo = document.querySelector(".open-two-screen");
-let storyMiddle = document.querySelector("story-middle");
-let middleButtons = document.querySelector("middle-buttons");
-let middleScreenOne = document.querySelector("middle-one-screen");
-let middleScreenTwo = document.querySelector("middle-two-screen");
-let endingOne = document.querySelector("option-one-end");
-let endingTwo = document.querySelector("option-two-end");
+let storyMiddle = document.querySelector(".story-middle");
+let middleButtons = document.querySelector(".middle-buttons");
+let middleScreenOne = document.querySelector(".middle-one-screen");
+let middleScreenTwo = document.querySelector(".middle-two-screen");
+let storyEnd = document.querySelector(".story-end");
+let endButtons = document.querySelector(".end-buttons");
+let endingOne = document.querySelector(".option-one-end");
+let endingTwo = document.querySelector(".option-two-end");
 
 
 
